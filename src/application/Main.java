5a6f0e4ac1ebe97fd9e9e485cf4eddc6b8e34d8d
Arnaldo Neto <vilela-neto@hotmail.com>
2020-12-31 13:51:49 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,27 +4,35 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//instancia o novo objeto "loader" do tipo FXMLLoader
+			// instancia o novo objeto "loader" do tipo FXMLLoader
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			//chama o load que carrega a view
-			Parent parent = loader.load();
+			// chama o load que carrega a view
+			ScrollPane scrollPane = loader.load();
+			
+			//ajusta o menubar ao tamanho da tela
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			
+			
 			// cria o objeto do tipo Scene, que vai ser a cena principal
-			//instancia a cena passando como argumento o objeto principal da view (que foi declarado acima) 
-			//[ que nesse caso vai ser o AnchorPane vazio 
-			Scene mainScene = new Scene(parent);
-			//faz o set da cena como sendo a mainScene declarada acima
+			// instancia a cena passando como argumento o objeto principal da view (que foi
+			// declarado acima)
+			// [ que nesse caso vai ser o AnchorPane vazio
+			Scene mainScene = new Scene(scrollPane);
+			// faz o set da cena como sendo a mainScene declarada acima
 			primaryStage.setScene(mainScene);
-			//Define o título do palco
+			// Define o título do palco
 			primaryStage.setTitle("Sample JavaFX application");
-			//exibe o palco
+			// exibe o palco
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
