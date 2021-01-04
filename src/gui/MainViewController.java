3998de1b7ca5bf0367d15127adfecb4c9d8d13bb
Arnaldo Neto <vilela-neto@hotmail.com>
2021-01-04 20:15:55 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,12 +36,18 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", 
+			(DepartmentListController controller) -> 
+				{
+					controller.setDepartmentService(new DepartmentService());
+					controller.updateTableView();
+				}
+		);
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
@@ -48,14 +55,18 @@ public class MainViewController implements Initializable {
 
 	}
 
-	//Objetivo deste método é carregar a outra view(tela) dentro da janela principal já existente
-	//para que possa trabalhar com a janela principal é preciso pegar uma referência da "cena" 
-	//...a "cena" foi declarada dentro do Main.java da seguinte forma: private static Scene mainScene;
+	// Objetivo deste método é carregar a outra view(tela) dentro da janela
+	// principal já existente
+	// para que possa trabalhar com a janela principal é preciso pegar uma
+	// referência da "cena"
+	// ...a "cena" foi declarada dentro do Main.java da seguinte forma: private
+	// static Scene mainScene;
 	//
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
-			
-			// carrega a visualização de outra tela na frente da tela principal (arquivo .fxml)
+
+			// carrega a visualização de outra tela na frente da tela principal (arquivo
+			// .fxml)
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
 
@@ -73,49 +84,17 @@ public class MainViewController implements Initializable {
 			Node mainMenu = mainVBox.getChildren().get(0);
 			// limpa todos os filhos do VBox do MainView.fxml
 			mainVBox.getChildren().clear();
-			//adiciona o mainMenu
+			// adiciona o mainMenu
 			mainVBox.getChildren().add(mainMenu);
-			//adiciona os filhos da janela que for abrir
+			// adiciona os filhos da janela que for abrir
 			mainVBox.getChildren().addAll(newVBox.getChildren());
-
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
-		try {
 			
-			// carrega a visualização de outra tela na frente da tela principal (arquivo .fxml)
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-
-			// Variável mainScene recebendo o valor da tela que está instanciada no momento
-			Scene mainScene = Main.getMainScene();
-			// pegando referência ao VBox da janela principal
-			// mainScene.getRoot() : pega o primeiro elemento do MainView.fxml (que é o
-			// ScrollPane)
-			// getContent pega o contaúdo dentro do ScrollPane (neste caso é a VBox)
-			// Desta forma pega-se uma referência para o VBox que está na janela principal
-			// (MainView.fxml)
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-
-			// primeiro filho do VBox da janela principal (que é o MenuBar do MainView.fxml)
-			Node mainMenu = mainVBox.getChildren().get(0);
-			// limpa todos os filhos do VBox do MainView.fxml
-			mainVBox.getChildren().clear();
-			//adiciona o mainMenu
-			mainVBox.getChildren().add(mainMenu);
-			//adiciona os filhos da janela que for abrir
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-
-			//O "loader" é o objeto declarado acima que é usado para 
-			//pega referência para o controller desta view
-			DepartmentListController controller = loader.getController();
-			//injeta dependência
-			controller.setDepartmentService(new DepartmentService());
-			//chama o método para atualizar os dados da tableView
-			controller.updateTableView();
+			
+			//o getController vai retornar um controlador do tipo que for chamado acima 
+			//por exemplo DepartmentListController
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+			
 			
 			
 		} catch (IOException e) {
@@ -124,8 +103,3 @@ public class MainViewController implements Initializable {
 	}
 
 }
-
-
-
-
-
