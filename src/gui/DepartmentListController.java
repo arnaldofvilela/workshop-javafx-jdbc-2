5140b109664ebf0ac2f96a49dec674d9aa41771c
Arnaldo Neto <vilela-neto@hotmail.com>
2021-01-15 19:34:35 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,81 +1,123 @@
- package gui;
+package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable{
+public class DepartmentListController implements Initializable {
 
 	private DepartmentService service;
-	
+
 	@FXML
 	private TableView<Department> tableViewDepartment;
-	
+
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
-	
+
 	@FXML
 	private Button btNew;
-	
+
 	private ObservableList<Department> obsList;
-	
+
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	//ActionEvent event -> destinado a ter uma referência do controle que receber o evento
+	public void onBtNewAction(ActionEvent event) {
+		//pega uma referência para o stage atual
+		Stage parentStage = Utils.currentStage(event);
+		//passa essa referência para criar a nova janela
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
-	
+
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
-		
+
 	}
 
 	private void initializeNodes() {
 
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
-		//faz com que a tableView acompanhe o tamanho da janela
-		//pega referência para o stage
+
+		// faz com que a tableView acompanhe o tamanho da janela
+		// pega referência para o stage
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
 
-	//acessa o serviço, carrega os departamentos 
+	// acessa o serviço, carrega os departamentos
 	// e coloca os departamentos dentro da "ObservableList<Department> obsList"
 	// então será feita a associação do ObservableList com o TableView
 	// e só então os departamentos aparecerão na exibição
 	public void updateTableView() {
 		//
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
-		//recupera os departamentos do serviço (os que estão no "mock")
+		// recupera os departamentos do serviço (os que estão no "mock")
 		List<Department> list = service.findAll();
-		//carregar a lista dentro do obsList
+		// carregar a lista dentro do obsList
 		obsList = FXCollections.observableArrayList(list);
-		//carrega os itens na tableView para que sejam mostrados na tela
+		// carrega os itens na tableView para que sejam mostrados na tela
 		tableViewDepartment.setItems(obsList);
-		
+
+	}
+
+	// no parâmetro é uma referência ao stage que criou a janela de diálogo
+	// para mostrar quem é o stage que criou a janela de diálogo
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			// carrega a visualização de outra tela na frente da tela principal (arquivo
+			// .fxml)
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			// carrega a view
+			Pane pane = loader.load();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			// define quem é o stage pai desta janela, neste caso o parentStage
+			// por esse motivo foi necessário pegar essa informação por parâmetro no início
+			// do método
+			dialogStage.initOwner(parentStage);
+			// faz a janela ser modal
+			// enquanto não fechar ela não consegue acessar a janela anterior
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+
+		} 
+		catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
---
+++ src/gui/util/Utils.java
@@ -0,0 +1,19 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	//palco atual = currentStage
+	//ActionEvent = evento do botão
+	//getSource é do tipo object -> muito genérico
+	//faz-se o downcasting dele para o tipo Node
+	//a partir desse node chama  a função getScene
+	//getWindow para pegar a janela
+	//essa janela é uma super classe do stage
+	//então faz-se o downcasting pra Stage
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage)((Node)event.getSource()).getScene().getWindow();
+	}
+}
